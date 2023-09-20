2863ed2c3e0a4fc306c168760cdee646c696deb1
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-09-19 22:33:52 -0400

    index.js and server.js

--- Ecommerce/Index.js
+++ Ecommerce/Index.js
@@ -1,6 +1,23 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to Ecommerce');
-})
+const handleRequest = (req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to Ecommerce');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+};
+
+const server = http.createServer(handleRequest);
+
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
