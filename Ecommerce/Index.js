08ea56ece80951e3ec3f42f96879e64e45fa45f4
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-09-19 23:26:00 -0400

    Ecommerce2 (#1)
    * server.js to read users.json
    
    * server.js to read json file
    
    * Update README.md
    
    used codeclimate and a read write file has been added to read the json file
    
    * Update README.md
    
    * index.js and server.js
    
    * Update package.json
    
    * Update package.json
    
    * Update Index.js
    
    * Update Index.js
    
    * index.js
    
    * package.json

--- Ecommerce/Index.js
+++ Ecommerce/Index.js
@@ -1,12 +1,32 @@
+// Import the 'http' module to create an HTTP server.
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to Ecommerce');
+// Define a function to handle incoming requests.
+const handleRequest = (req, res) => {
+  // Check if the request method is 'GET' and the URL is the root ('/').
+  if (req.method === 'GET' && req.url === '/') {
+    // If it is a valid request, respond with a '200 OK' status and a plain text message.
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Welcome to Ecommerce');
+  } else {
+    // If the request is for a different URL or method, respond with a '404 Not Found' status.
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+};
+
+// Define the port number. Use the value from the environment variable 'PORT', or default to 3001.
+const PORT = process.env.PORT || 3000;
+
+// Create an HTTP server and pass the request handling function to it.
+const server = http.createServer(handleRequest);
+
+// Start the server on the specified port and log a message to the console.
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const PORT = 3001;
-
-server.listen(PORT, () => {
-  console.log(`Server running on <http://localhost>:${PORT}`);
+// Add an error event listener to handle server-related errors.
+server.on('error', (err) => {
+  console.error('Server error:', err);
 });
