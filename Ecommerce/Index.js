7388927c758f4012d4c2e564a7ca3468ebcf3690
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-09-19 21:41:01 -0400

    server.js to read users.json

--- Ecommerce/Index.js
+++ Ecommerce/Index.js
@@ -3,10 +3,4 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Welcome to Ecommerce');
-});
-
-const PORT = 3001;
-
-server.listen(PORT, () => {
-  console.log(`Server running on <http://localhost>:${PORT}`);
-});
+})
