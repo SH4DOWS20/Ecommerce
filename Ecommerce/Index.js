53351d0f0eb3b1e67146444b1cf2474cfdacc06a
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-11-03 20:47:22 -0400

    not sure what got changed

--- Ecommerce/Index.js
+++ Ecommerce/Index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
+const PORT = process.env.PORT || 3008;
 
 // Middleware setup
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
@@ -104,81 +105,81 @@ app.delete('/api/cart/delete/:id', (req, res) => {
 // Create an HTTP server and pass the Express app to it.
 
 const Cart = [
-    { id: 1, name: 'Limited IceDog hat' },
-    { id: 2, name: 'Navy blue beanie' },
-    { id: 3, name: 'Green baseball cap' },
+  { id: 1, name: 'Limited IceDog hat' },
+  { id: 2, name: 'Navy blue beanie' },
+  { id: 3, name: 'Green baseball cap' },
 ];
 
 app.get('/', (req, res) => {
-    res.send(`<button><a href="/cart">Cart</a></button> <button><a href="/addCart">Add Cart</a></button>`);
+  res.send(`<button><a href="/cart">Cart</a></button> <button><a href="/addCart">Add Cart</a></button>`);
 });
 
 app.get('/api/cart', async (req, res) => {
-    try {
-        const delayedData = new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(Cart);
-            }, 2000);
-        });
+  try {
+    const delayedData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Cart);
+      }, 2000);
+    });
 
-        const result = await delayedData;
-        res.render("index", { Cart: result });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`${error}`);
-    }
+    const result = await delayedData;
+    res.render("index", { Cart: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`${error}`);
+  }
 });
 
 app.get('/api/cart/add', (req, res) => {
-    res.render('addCart.ejs');
+  res.render('addCart.ejs');
 });
 
 app.get('/api/cart/add/:id', (req, res) => {
-    const CartId = parseInt(req.params.id);
-    const cartItem = Cart.find(item => item.id == CartId);
-    if (cartItem) {
-        res.render('updateCart.ejs', { cartItem: cartItem });
-    } else {
-        res.status(404).send(`Cart with ID ${CartId} not found.`);
-    }
+  const CartId = parseInt(req.params.id);
+  const cartItem = Cart.find(item => item.id == CartId);
+  if (cartItem) {
+    res.render('updateCart.ejs', { cartItem: cartItem });
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
 });
 
 app.post('/api/cart', (req, res) => {
-    console.log(req.body.name);
+  console.log(req.body.name);
 
-    const newCart = {
-        id: Cart.length + 1,
-        name: req.body.name,
-    };
+  const newCart = {
+    id: Cart.length + 1,
+    name: req.body.name,
+  };
 
-    Cart.push(newCart);
-    res.redirect('/api/cart');
+  Cart.push(newCart);
+  res.redirect('/api/cart');
 });
 
 app.post('/api/cart/delete/:id', (req, res) => {
-    const CartId = parseInt(req.params.id);
-    const index = Cart.findIndex(item => item.id == CartId);
-    if (index !== -1) {
-        Cart.splice(index, 1);
-        res.redirect('/api/cart/');
-    } else {
-        res.status(404).send(`Cart with ID ${CartId} not found.`);
-    }
+  const CartId = parseInt(req.params.id);
+  const index = Cart.findIndex(item => item.id == CartId);
+  if (index !== -1) {
+    Cart.splice(index, 1);
+    res.redirect('/api/cart/');
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
 });
 
 app.post('/api/cart/update/:id', (req, res) => {
-    const CartId = parseInt(req.params.id);
-    const updateName = req.body.name;
-    const cartItem = Cart.find(item => item.id == CartId);
+  const CartId = parseInt(req.params.id);
+  const updateName = req.body.name;
+  const cartItem = Cart.find(item => item.id == CartId);
 
-    if (cartItem) {
-        cartItem.name = updateName;
-        res.redirect('/api/cart');
-    } else {
-        res.status(404).send(`Cart with ID ${CartId} not found.`);
-    }
+  if (cartItem) {
+    cartItem.name = updateName;
+    res.redirect('/api/cart');
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
--- Ecommerce/frontend/src/App.js
+++ Ecommerce/frontend/src/App.js
@@ -1,52 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import CartList from './components/CartList';
-import React, { useState, useEffect } from 'react';
-import { randomhatpurchase } from './randomhatpurchse';
+import RandomHatPurchase from './randomhatpurchse';
 
 function App() {
   const [cart, setCart] = useState([]);
   const [newItem, setNewItem] = useState('');
 
-  useEffect(() => {
-    // Fetch data from the server and update the state
-    fetch('/api/cart')
-      .then((response) => response.json())
-      .then((data) => setCart(data))
-      .catch((error) => console.error(error));
-  }, []);
-  //
-  function randomhatpurchase() {
-    const [hat, setHat] = useState(0);
-  
-    useEffect(() => {
-      document.title = `Hats: ${hats}`;
-    }, [hats]);
-  
-    const incrementSteps = () => {
-      setSteps(steps + 1);
-    };
-    return (
-      <div>
-        <h1>IceDog</h1>
-        <p>How many Random Hats would you like to purchse?: {steps}</p>
-        <button onClick={incrementSteps}>Add Random Hat</button>
-        {steps >= 10 && <p>You've reached the maximum amount of hats that can be purchased</p>}
-      </div>
-    )
-}  
-//
+  // Implement the delete functionality here
   const handleDelete = (id) => {
-    // Implement the delete functionality here
+    const updatedCart = cart.filter(item => item.id !== id);
+    setCart(updatedCart);
   };
 
-  const handleUpdate = (id) => {
-    // Implement the update functionality here
+  // Implement the update functionality here
+  const handleUpdate = (id, updatedItem) => {
+    const updatedCart = cart.map(item => item.id === id ? updatedItem : item);
+    setCart(updatedCart);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Assuming newItem is an object representing a cart item
-    setCart([...cart, newItem]);
+    // Create a new item object and add it to the cart
+    const newItemObject = {
+      id: cart.length + 1, // You should use a proper ID generation mechanism
+      name: newItem,
+    };
+    setCart([...cart, newItemObject]);
     setNewItem('');
   };
 
@@ -65,6 +44,9 @@ function App() {
         />
         <button type="submit">Add Item to Cart</button>
       </form>
+
+      <h2>Random Hat Purchase</h2>
+      <RandomHatPurchase />
     </div>
   );
 }
