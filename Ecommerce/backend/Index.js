0824b5497de490b338087427ae195410acf71a3e
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-11-07 17:53:20 -0500

    lab6 hooks, complex state and conditional renderin

---
+++ Ecommerce/backend/Index.js
@@ -0,0 +1,236 @@
+// Import the required modules
+const http = require('http');
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const cors = require('cors');
+const { url } = require('inspector');
+
+// Middleware setup
+app.use(cors());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+app.set('views', __dirname + '/views'); // Set the views directory (adjust the path as needed)
+app.use(methodOverride('_method')); // Override HTTP methods using a query parameter
+
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+
+// Sample cart data
+const cart = [
+  { id: 1, name: 'Exclusive ice hat' },
+  { id: 2, name: 'limited dog hat' },
+  { id: 3, name: 'rare ice dog hat' },
+];
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send(`
+    <button><a href="/api/cart">Cart</a></button>
+    <button><a href="/api/cart/add">Add to cart</a></button>
+  `);
+});
+
+// Route for adding items to the cart
+app.get('/api/cart/add', (req, res) => {
+  res.render("Browse", { cart: cart });
+});
+
+// Route for adding items to the cart by ID
+app.get('/api/cart/add/:id', (req, res) => {
+  res.render('Addtocart'); // Assuming you have an 'Addtocart.ejs' template in the 'views' directory
+});
+
+app.get('/api/workouts', (req, res) => {
+  const collection = db.collection('workouts');
+
+  collection.find({}).toArray((err, workouts) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error fetching from database');
+      return;
+    }
+    res.json(workouts);
+  });
+});
+
+// Route for handling POST requests to add items to the cart
+app.post('/api/cart', (req, res) => {
+  console.log(req.body.name);
+
+  const newcart = {
+    id: cart.length + 1,
+    name: req.body.name,
+  };
+
+  cart.push(newcart);
+  Cart.push(newcart);
+  res.redirect('/api/cart');
+});
+
+// Route for updating cart items by ID
+app.put('/api/cart/update/:id', (req, res) => {
+  console.log("fired put");
+  const cartId = parseInt(req.params.id);
+  const updatedName = req.body.name;
+
+  const cartItem = cart.find(c => c.id === cartId);
+
+  if (cartItem) {
+    cartItem.name = updatedName;
+    res.status(200).send(`Cart with ID ${cartId} updated.`);
+  } else {
+    res.status(404).send(`Cart with ID ${cartId} not found.`);
+  }
+});
+
+// Route for deleting cart items by ID
+app.delete('/api/cart/delete/:id', (req, res) => {
+  const cartId = parseInt(req.params.id);
+
+  const index = cart.findIndex(c => c.id === cartId);
+
+  if (index !== -1) {
+    cart.splice(index, 1);
+    res.redirect('/api/cart');
+  } else {
+    res.status(404).send(`Cart with ID ${cartId} not found.`);
+  }
+});
+
+// Create an HTTP server and pass the Express app to it.
+
+
+// Define the port number. Use the value from the environment variable 'PORT', or default to 3008.
+const PORT = process.env.PORT || 3008;
+
+const Cart = [
+  { id: 1, name: 'Limited IceDog hat' },
+  { id: 2, name: 'Navy blue beanie' },
+  { id: 3, name: 'Green baseball cap' },
+];
+
+app.get('/', (req, res) => {
+  res.send(`<button><a href="/cart">Cart</a></button> <button><a href="/addCart">Add Cart</a></button>`);
+});
+
+
+app.get('/api/cart', async (req, res) => {
+  try {
+    const delayedData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Cart);
+      }, 2000);
+    });
+
+    const result = await delayedData;
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`${error}`);
+  }
+});
+
+app.get('/api/cart', async (req, res) => {
+  try {
+    const delayedData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(Cart);
+      }, 2000);
+    });
+
+    const result = await delayedData;
+    res.render("Cart", { Cart: result });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`${error}`);
+  }
+});
+
+app.get('/api/cart/add/:id', (req, res) => {
+  const CartId = parseInt(req.params.id);
+  const cartItem = Cart.find(item => item.id == CartId);
+  if (cartItem) {
+    res.render('updateCart.ejs', { cartItem: cartItem });
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
+});
+
+// Route for adding a new item to the cart
+app.post('/api/cart/add', (req, res) => {
+  console.log(req.body.name);
+
+  const newItem = {
+    id: cart.length + 1,
+    name: req.body.name,
+  };
+
+  cart.push(newItem);
+  res.redirect('/api/cart');
+});
+
+app.post('/api/cart', (req, res) => {
+  console.log(req.body.name);
+
+  const newCart = {
+    id: Cart.length + 1,
+    name: req.body.name,
+  };
+
+  Cart.push(newCart);
+  res.redirect('/api/cart');
+});
+
+app.post('/api/cart/delete/:id', (req, res) => {
+  const CartId = parseInt(req.params.id);
+  const index = Cart.findIndex(item => item.id == CartId);
+  if (index !== -1) {
+    Cart.splice(index, 1);
+    res.json(Cart);
+    res.redirect('/api/cart/');
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
+});
+
+app.post('/api/cart/update/:id', (req, res) => {
+  const CartId = parseInt(req.params.id);
+  const updateName = req.body.name;
+  const cartItem = Cart.find(item => item.id == CartId);
+
+  if (cartItem) {
+    cartItem.name = updateName;
+    res.redirect('/api/cart');
+  } else {
+    res.status(404).send(`Cart with ID ${CartId} not found.`);
+  }
+});
+
+app.post('/api/Cart', (req, res) => {
+  const { name, duration } = req.body;
+
+  const newCart = {
+    name,
+    duration: parseInt(duration),
+  };
+
+  const collection = db.collection('Cart');
+  collection.insertOne(newWorkout, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error saving to database');
+      return;
+    }
+    console.log('Saved to database');
+    res.redirect('/Addtocart');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
--- Ecommerce/frontend/src/App.js
+++ Ecommerce/frontend/src/App.js
@@ -1,58 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import CartList from './components/CartList';
-import React, { useState, useEffect } from 'react';
-import { randomhatpurchase } from './randomhatpurchse';
+import React, { useState, useEffect, useReducer } from 'react';
+import CartList from './CartList';
+
+// Reducer function to manage cart state
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_CART':
+      return action.payload;
+    case 'ADD_ITEM':
+      return [...state, action.payload];
+    case 'DELETE_ITEM':
+      return state.filter(item => item.id !== action.payload);
+    case 'UPDATE_ITEM':
+      return state.map(item => (item.id === action.payload.id ? action.payload : item));
+    default:
+      return state;
+  }
+};
 
 function App() {
-  const [cart, setCart] = useState([]);
+  const [cart, dispatch] = useReducer(cartReducer, []);
   const [newItem, setNewItem] = useState('');
 
   useEffect(() => {
-    // Fetch data from the server and update the state
-    fetch('/api/cart')
-      .then((response) => response.json())
-      .then((data) => setCart(data))
-      .catch((error) => console.error(error));
+    // Fetch data from the server and update the cart state
+    fetch('http://localhost:3008/api/cart')
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'SET_CART', payload: data });
+        console.log(data);
+      })
+      .catch(error => console.error(error));
   }, []);
-  //
-  function randomhatpurchase() {
-    const [hat, setHat] = useState(0);
-  
-    useEffect(() => {
-      document.title = `Hats: ${hats}`;
-    }, [hats]);
-  
-    const incrementSteps = () => {
-      setSteps(steps + 1);
-    };
-    return (
-      <div>
-        <h1>IceDog</h1>
-        <p>How many Random Hats would you like to purchse?: {steps}</p>
-        <button onClick={incrementSteps}>Add Random Hat</button>
-        {steps >= 10 && <p>You've reached the maximum amount of hats that can be purchased</p>}
-      </div>
-    )
-}  
-//
-  const handleDelete = (id) => {
-    // Implement the delete functionality here
+
+  const handleDelete = id => {
+    fetch(`http://localhost:3008/api/cart/delete/${id}`, { method: 'POST' })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'DELETE_ITEM', payload: id });
+        console.log(data);
+      })
+      .catch(error => console.error(error));
   };
 
-  const handleUpdate = (id) => {
-    // Implement the update functionality here
+  const handleUpdate = (id, updatedData) => {
+    fetch(`http://localhost:3008/api/cart/update/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'UPDATE_ITEM', payload: data });
+        console.log(data);
+      })
+      .catch(error => console.error(error));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    // Assuming newItem is an object representing a cart item
-    setCart([...cart, newItem]);
+
+    // Create an object representing the new item
+    const newItemData = { name: newItem };
+
+    // Send a POST request to add the new item to the cart
+    fetch('http://localhost:3008/api/cart/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newItemData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch({ type: 'ADD_ITEM', payload: data });
+        console.log(data);
+      })
+      .catch(error => console.error(error));
+
+    // Clear the input field
     setNewItem('');
   };
 
   return (
     <div>
       <h1>Cart Items</h1>
+      {cart.length === 0 ? (
+        <p>Your cart is empty. Add items below:</p>
+      ) : null}
       <CartList cart={cart} onDelete={handleDelete} onUpdate={handleUpdate} />
 
       <h2>Add Item to Cart</h2>
@@ -61,7 +97,7 @@ function App() {
           type="text"
           placeholder="Type an item"
           value={newItem}
-          onChange={(e) => setNewItem(e.target.value)}
+          onChange={e => setNewItem(e.target.value)}
         />
         <button type="submit">Add Item to Cart</button>
       </form>
