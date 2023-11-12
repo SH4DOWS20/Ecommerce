725a2742344a18bf718f33daffc9e78a77517ff6
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-11-12 14:18:57 -0500

    Ecommerce3 (#18)
    * Ecommerce fixed website
    
    * Update README.md
    
    * merge with master

---
+++ Ecommerce/Index.js
@@ -0,0 +1,119 @@
+// Import the 'http' module to create an HTTP server.
+const http = require('http');
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+
+// Middleware setup
+app.use(express.urlencoded()); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+app.set('view engine', 'ejs'); // Set the view engine to EJS
+app.use(methodOverride('_method')); // Override HTTP methods using a query parameter
+
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
+function handleRequest(req, res) { }
+// Sample cart data
+const cart = [
+  { id: 1, name: 'Exclusive ice hat' },
+  { id: 2, name: 'limited dog hat' },
+  { id: 3, name: 'rare ice dog hat' },
+];
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send(`
+    <button><a href="/api/cart">Cart</a></button>
+    <button><a href="/api/cart/add">Add to cart</a></button>
+  `);
+});
+
+// Route for fetching cart data
+app.get('/api/cart', async (req, res) => {
+  try {
+    // Simulate a delay in fetching data
+    const delayedData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(cart);
+      }, 2000);
+    });
+    const result = await delayedData;
+    res.json(result);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Route for adding items to the cart
+app.get('/api/cart/add', (req, res) => {
+  res.render('Browse.ejs');
+});
+
+// Route for adding items to the cart by ID
+app.get('/api/cart/add/:id', (req, res) => {
+  res.render('Addtocart.ejs');
+});
+
+// Route for handling POST requests to add items to the cart
+app.post('/api/cart', (req, res) => {
+  console.log(req.body.name);
+
+  const newcart = {
+    id: cart.length + 1,
+    name: req.body.name,
+  };
+
+  cart.push(newcart);
+  res.redirect('/api/cart');
+});
+
+// Route for updating cart items by ID
+app.put('/api/cart/update/:id', (req, res) => {
+  console.log("fired put");
+  const cartId = parseInt(req.params.id);
+  const updatedName = req.body.name;
+
+  const cartItem = cart.find(c => c.id === cartId);
+
+  if (cartItem) {
+    cartItem.name = updatedName;
+    res.status(200).send(`cart with ID ${cartId} updated.`);
+  } else {
+    res.status(404).send(`cart with ID ${cartId} not found.`);
+  }
+});
+
+// Route for deleting cart items by ID
+app.delete('/api/cart/delete/:id', (req, res) => {
+  const cartId = parseInt(req.params.id);
+
+  const index = cart.findIndex(c => c.id === cartId);
+
+  if (index !== -1) {
+    cart.splice(index, 1);
+    res.redirect('/api/cart');
+  } else {
+    res.status(404).send(`cart with ID ${cartId} not found.`);
+  }
+});
+
+// Create an HTTP server and pass the request handling function to it.
+
+const server = http.createServer(handleRequest);
+
+// Define the port number. Use the value from the environment variable 'PORT', or default to 3008.
+const PORT = process.env.PORT || 3008;
+
+// Start the server on the specified port and log a message to the console.
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Add an error event listener to handle server-related errors.
+server.on('error', (err) => {
+  console.error('Server error:', err);
+});
+
--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-Mini Project 2
-- Filesystem Module
-- Path Module
-- HTTP Module
-- REST APIs
-- Postman
-- Arrow Functions
-- Node Features
+Mini-Project 3: RESTful APIs
+- Advanced RESTful API Design
+- API Versioning
+- Query Parameters
+- Pagination
+- Error Handling
 <a href="https://codeclimate.com/github/SH4DOWS20/Ecommerce/maintainability"><img src="https://api.codeclimate.com/v1/badges/c5887f6279a2fe693cee/maintainability" /></a>
 <a href="https://codeclimate.com/github/SH4DOWS20/Ecommerce/test_coverage"><img src="https://api.codeclimate.com/v1/badges/c5887f6279a2fe693cee/test_coverage" /></a>
