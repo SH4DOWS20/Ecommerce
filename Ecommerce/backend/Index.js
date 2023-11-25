fdaeeae6bb12a38bef4f44366ce8f77d43ae629f
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-11-25 14:38:07 -0500

    Ecommerce lab7 (#22)
    * Ecommerce fixed website
    
    * added async, promises, callbacks, event loops
    
    * added usestatehooks and lab requirements
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * code climate
    
    * Update Index.js
    
    * please work
    
    * added custom hooks and conditional rendering
    
    * lab6 hooks, complex state and conditional renderin
    
    * add to master
    
    * add to master
    
    * merge into master
    
    * merge with master
    
    * merge with master
    
    * merge with master
    
    * Update README.md
    
    * Delete READme.md
    
    * merge with master
    
    * merge with master

--- Ecommerce/backend/Index.js
+++ Ecommerce/backend/Index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
-
+import methodOverride from 'method-override';
+import { MongoClient } from 'mongodb';
 // Middleware setup
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
 app.use(express.json()); // Parse JSON bodies
@@ -16,6 +17,19 @@ app.use((req, res, next) => {
   console.log(`${req.method} request for ${req.url}`);
   next();
 });
+
+let db;
+
+(async function() {
+    try {
+      const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
+      console.log('Connected to MongoDB.');
+      db = client.db("IceDog");
+
+    } catch (err) {
+      console.error('Error occurred while connecting to MongoDB:', err);
+    }
+})();
 
 // Sample cart data
 const cart = [
@@ -31,6 +45,18 @@ app.get('/', (req, res) => {
     <button><a href="/api/cart/add">Add to cart</a></button>
   `);
 });
+
+app.get('/api/Cart', async (req, res) => {
+  try {
+      const collection = db.collection('browse');
+      const workouts = await collection.find({}).toArray();
+      res.render("workouts",{workouts});
+  } catch (err) {
+      console.log(err);
+      res.status(500).send('Error fetching from database');
+  }
+});
+
 
 // Route for fetching cart data
 app.get('/api/cart', async (req, res) => {
@@ -69,6 +95,26 @@ app.post('/api/cart', (req, res) => {
 
   cart.push(newcart);
   res.redirect('/api/cart');
+});
+
+app.post('/api/Cart', (req, res) => {
+  const { name, duration } = req.body;
+
+  const newCart = {
+    name,
+    duration: parseInt(duration),
+  };
+
+  const collection = db.collection('Cart');
+  collection.insertOne(newWorkout, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error saving to database');
+      return;
+    }
+    console.log('Saved to database');
+  });
+  res.redirect('/api/Cart');
 });
 
 // Route for updating cart items by ID
