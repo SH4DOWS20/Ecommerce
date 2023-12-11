c10bf489804bb1ba6728fac74bc7e6cc6e345b75
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-12-10 22:17:04 -0500

    soon to be merged

--- Ecommerce/backend/Index.js
+++ Ecommerce/backend/Index.js
@@ -4,28 +4,24 @@ import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
 import mongoose from 'mongoose';
 import methodOverride from 'method-override';
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import bcrypt from 'bcrypt';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
 const app = express();
 
 // Middleware setup
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(bodyParser.json()); // Parse JSON bodies
-app.set('view engine', 'ejs'); // Set the view engine to EJS
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.set('view engine', 'ejs');
 
-// Use the updated __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 app.set('views', `${__dirname}/views`);
-
-app.use(methodOverride('_method')); // Override HTTP methods using a query parameter
-
-// Request logging middleware
-app.use((req, res, next) => {
-  console.log(`${req.method} request for ${req.url}`);
-  next();
-});
 
 // MongoDB connection
 let db;
@@ -45,10 +41,65 @@ let db;
 const cartItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, default: 1 },
-  // Add more fields as needed
 });
 
 const CartItem = mongoose.model('CartItem', cartItemSchema);
+
+// Mongoose User Schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+const User = mongoose.model('User', userSchema);
+
+// Passport.js configuration
+passport.use(new LocalStrategy(
+  { usernameField: 'username' },
+  async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+
+      const passwordMatch = await bcrypt.compare(password, user.password);
+
+      if (!passwordMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+      return done(null, user);
+    } catch (error) {
+      return done(error);
+    }
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+// Express session setup
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} request for ${req.url}`);
+  next();
+});
 
 // Create a new cart item
 app.post('/api/cart', async (req, res) => {
@@ -131,11 +182,74 @@ app.delete('/api/cart/:id', async (req, res) => {
   }
 });
 
+// Registration route
+app.get('/registration', (req, res) => {
+  res.render('registration'); // You can create a registration.ejs file for the registration form
+});
+
+app.post('/registration', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: await bcrypt.hash(password, 10), // Hash the password before saving
+    });
+
+    const savedUser = await newUser.save();
+    res.redirect('/login'); // Redirect to the login page after successful registration
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Login route
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+  })
+);
+
+// Logout route
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+// Ensure authentication for certain routes
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+// Protected route (example)
+app.get('/profile', ensureAuthenticated, (req, res) => {
+  res.send(`Welcome, ${req.user.username}!`);
+});
+
 // Route for the root URL
 app.get('/', (req, res) => {
   res.send(`
     <button><a href="/api/cart">Cart</a></button>
     <button><a href="/api/cart/add">Add to Cart</a></button>
+    <button><a href="/login">Login</a></button>
+    <button><a href="/logout">Logout</a></button>
+    <button><a href="/registration">Registration</a></button> <!-- Added registration link -->
   `);
 });
 
@@ -152,5 +266,5 @@ server.listen(PORT, () => {
 
 // Add an error event listener to handle server-related errors.
 server.on('error', (err) => {
-  console.error('Server error:', err);
+  console.error(err);
 });
