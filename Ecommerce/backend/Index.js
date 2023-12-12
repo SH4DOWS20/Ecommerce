be9c68503ec36b5cd5554c03a02db224583ad041
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-12-11 23:59:36 -0500

    Ecommerce lab9 (#27)
    * Ecommerce fixed website
    
    * added async, promises, callbacks, event loops
    
    * added usestatehooks and lab requirements
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * Update Index.js
    
    * code climate
    
    * Update Index.js
    
    * please work
    
    * added custom hooks and conditional rendering
    
    * lab6 hooks, complex state and conditional renderin
    
    * add to master
    
    * add to master
    
    * merge into master
    
    * merge with master
    
    * merge with master
    
    * merge with master
    
    * Update README.md
    
    * Delete READme.md
    
    * merge with master
    
    * merge with master
    
    * merge with master
    
    * commit
    
    * merge with master
    
    * merge with master
    
    * soon to merge
    
    * soon to be merged
    
    * merge with master

--- Ecommerce/backend/Index.js
+++ Ecommerce/backend/Index.js
@@ -1,18 +1,21 @@
-// Import the required modules
-import { MongoClient } from 'mongodb';
-import mongoose from 'mongoose';
 import express from 'express';
 import http from 'http';
-import methodOverride from 'method-override';
 import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
+import methodOverride from 'method-override';
+import session from 'express-session';
+import passport from 'passport';
+import LocalStrategy from 'passport-local';
+import bcrypt from 'bcrypt';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 const app = express();
-const methodOverride = require('method-override');
-import methodOverride from 'method-override';
-import { MongoClient } from 'mongodb';
+
 // Middleware setup
-app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded bodies
-app.use(bodyParser.json()); // Parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
 app.set('view engine', 'ejs'); // Set the view engine to EJS
 app.set('views', __dirname + '/views'); // Set the views directory (adjust the path as needed)
 app.use(methodOverride('_method')); // Override HTTP methods using a query parameter
@@ -23,114 +26,132 @@ app.use((req, res, next) => {
   next();
 });
 
-// MongoDB connection
-(async function () {
-  try {
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true });
-    console.log('Connected to MongoDB.');
-    const db = client.db("Ecommerce");
-    mongoose.connect('mongodb://localhost:27017/Ecommerce', { useNewUrlParser: true, useUnifiedTopology: true });
-  } catch (err) {
-    console.error('Error occurred while connecting to MongoDB:', err);
-  }
+let db;
+
+(async function() {
+    try {
+      const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
+      console.log('Connected to MongoDB.');
+      db = client.db("IceDog");
+
+    } catch (err) {
+      console.error('Error occurred while connecting to MongoDB:', err);
+    }
 })();
 
-// Mongoose Cart Schema
-const cartItemSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  quantity: { type: Number, default: 1 },
-  // Add more fields as needed
+// Sample cart data
+const cart = [
+  { id: 1, name: 'Exclusive ice hat' },
+  { id: 2, name: 'limited dog hat' },
+  { id: 3, name: 'rare ice dog hat' },
+];
+
+// Route for the root URL
+app.get('/', (req, res) => {
+  res.send(`
+    <button><a href="/api/cart">Cart</a></button>
+    <button><a href="/api/cart/add">Add to cart</a></button>
+  `);
 });
 
-const CartItem = mongoose.model('CartItem', cartItemSchema);
-
-// Create a new cart item
-app.post('/api/cart', async (req, res) => {
+app.get('/api/Cart', async (req, res) => {
   try {
-    const { name, quantity } = req.body;
-
-    const newCartItem = new CartItem({
-      name,
-      quantity: quantity || 1,
-    });
-
-    const savedCartItem = await newCartItem.save();
-    res.status(201).json(savedCartItem);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+      const collection = db.collection('browse');
+      const workouts = await collection.find({}).toArray();
+      res.render("workouts",{workouts});
+  } catch (err) {
+      console.log(err);
+      res.status(500).send('Error fetching from database');
   }
 });
 
 // Get all cart items
 app.get('/api/cart', async (req, res) => {
   try {
-    const cartItems = await CartItem.find();
-    res.json(cartItems);
+    // Simulate a delay in fetching data
+    const delayedData = new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(cart);
+      }, 2000);
+    });
+    const result = await delayedData;
+    res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
 
-// Get a specific cart item by ID
-app.get('/api/cart/:id', async (req, res) => {
-  try {
-    const cartItemId = req.params.id;
-    const cartItem = await CartItem.findById(cartItemId);
+// Route for adding items to the cart
+app.get('/api/cart/add', (req, res) => {
+  res.render('Browse'); // Assuming you have a 'Browse.ejs' template in the 'views' directory
+});
 
-    if (!cartItem) {
-      return res.status(404).json({ error: 'Cart item not found' });
+// Route for adding items to the cart by ID
+app.get('/api/cart/add/:id', (req, res) => {
+  res.render('Addtocart'); // Assuming you have an 'Addtocart.ejs' template in the 'views' directory
+});
+
+// Route for handling POST requests to add items to the cart
+app.post('/api/cart', (req, res) => {
+  console.log(req.body.name);
+
+  const newcart = {
+    id: cart.length + 1,
+    name: req.body.name,
+  };
+
+  cart.push(newcart);
+  res.redirect('/api/cart');
+});
+
+app.post('/api/Cart', (req, res) => {
+  const { name, duration } = req.body;
+
+  const newCart = {
+    name,
+    duration: parseInt(duration),
+  };
+
+  const collection = db.collection('Cart');
+  collection.insertOne(newWorkout, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send('Error saving to database');
+      return;
     }
+    console.log('Saved to database');
+  });
+  res.redirect('/api/Cart');
+});
 
-    res.json(cartItem);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+// Route for updating cart items by ID
+app.put('/api/cart/update/:id', (req, res) => {
+  console.log("fired put");
+  const cartId = parseInt(req.params.id);
+  const updatedName = req.body.name;
+
+  const cartItem = cart.find(c => c.id === cartId);
+
+  if (cartItem) {
+    cartItem.name = updatedName;
+    res.status(200).send(`Cart with ID ${cartId} updated.`);
+  } else {
+    res.status(404).send(`Cart with ID ${cartId} not found.`);
   }
 });
 
-// Update a cart item by ID
-app.put('/api/cart/:id', async (req, res) => {
-  try {
-    const cartItemId = req.params.id;
-    const { name, quantity } = req.body;
+// Route for deleting cart items by ID
+app.delete('/api/cart/delete/:id', (req, res) => {
+  const cartId = parseInt(req.params.id);
 
-    const updatedCartItem = await CartItem.findByIdAndUpdate(
-      cartItemId,
-      { name, quantity },
-      { new: true } // Return the updated document
-    );
+  const index = cart.findIndex(c => c.id === cartId);
 
-    if (!updatedCartItem) {
-      return res.status(404).json({ error: 'Cart item not found' });
-    }
-
-    res.json(updatedCartItem);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
+  if (index !== -1) {
+    cart.splice(index, 1);
+    res.redirect('/api/cart');
+  } else {
+    res.status(404).send(`Cart with ID ${cartId} not found.`);
   }
-});
-
-// Delete a cart item by ID
-app.delete('/api/cart/:id', async (req, res) => {
-  try {
-    const cartItemId = req.params.id;
-    const deletedCartItem = await CartItem.findByIdAndRemove(cartItemId);
-
-    if (!deletedCartItem) {
-      return res.status(404).json({ error: 'Cart item not found' });
-    }
-
-    res.json(deletedCartItem);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Route for the root URL
-app.get('/', (req, res) => {
-  res.send(`
-    <button><a href="/api/cart">Cart</a></button>
-    <button><a href="/api/cart/add">Add to Cart</a></button>
-  `);
 });
 
 // Create an HTTP server and pass the Express app to it.
@@ -146,5 +167,5 @@ server.listen(PORT, () => {
 
 // Add an error event listener to handle server-related errors.
 server.on('error', (err) => {
-  console.error('Server error:', err);
+  console.error(err);
 });
