f776e33ee32efff9a2da5b91df41fad7952051e7
SH4DOWS20 <118479715+SH4DOWS20@users.noreply.github.com>
2023-12-13 17:20:47 -0500

    master

--- Ecommerce/frontend/src/App.jsx
+++ Ecommerce/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import axios from 'axios'
 
